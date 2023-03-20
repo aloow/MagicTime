79fd2a6ca232a5ac36fac8cb5b0876bf6e15a1b9
dfds <xuesq@pvc123.com>
2023-03-20 17:42:45 +0800

    Initial Commit

---
+++ MagicTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicTime
+//
+//  Created by iMac on 2023/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
